51d9bff8f253a18fc92b5882aa738d60b5961a84
Danila200160 <Danila200160@yandex.ru>
2022-07-09 23:29:08 +0300

    Version 3.0

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -45,6 +45,9 @@
             this.TestSave = new System.Windows.Forms.Button();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -174,7 +177,7 @@
             // 
             // TestSave
             // 
-            this.TestSave.Location = new System.Drawing.Point(242, 350);
+            this.TestSave.Location = new System.Drawing.Point(242, 349);
             this.TestSave.Name = "TestSave";
             this.TestSave.Size = new System.Drawing.Size(75, 23);
             this.TestSave.TabIndex = 19;
@@ -200,11 +203,33 @@
             this.label7.TabIndex = 21;
             this.label7.Text = "Barcode";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "&Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(523, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Autosave";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(783, 510);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.TestSave);
@@ -247,6 +272,9 @@
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,11 +23,13 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            
         }
 
+        string path;
+        int index = 0;
         private void Form1_Load(object sender, EventArgs e)
-        {
-
+        {            
         }       
        
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -43,45 +46,54 @@ namespace WindowsFormsApp1
         {
             int W = Convert.ToInt32(this.textBox3.Text.Trim());
             int H = Convert.ToInt32(this.textBox4.Text.Trim());
-
-
-
             BarcodeLib.Barcode b = new BarcodeLib.Barcode();
             Color forecolor = Color.Black;
             Color backcolor = Color.White;
             Image img = b.Encode(BarcodeLib.TYPE.CODE128, textBox1.Text, forecolor, backcolor, (int)(W), (int)(H));
-            pictureBox1.Image = img;
-            //BarcodeWriter writer = new BarcodeWriter()
-            //{
-            //    Format = BarcodeFormat.CODE_128,
-            //    Options = new ZXing.Common.EncodingOptions
-            //    {
-            //        Width = 200,
-            //        Height = 100
-            //    }
-            //};
-            //pictureBox1.Height = H;
-            //pictureBox1.Width = W;
-            //pictureBox1.Image = writer.Write(textBox1.Text);            
+            pictureBox1.Image = img;                      
             label6.Text = textBox1.Text;
             BarcodeLib.Barcode b1 = new BarcodeLib.Barcode();           
             Image img1 = b1.Encode(BarcodeLib.TYPE.CODE128, textBox6.Text, forecolor, backcolor, (int)(W), (int)(H));
             pictureBox2.Image = img1;
             label8.Text = textBox6.Text;
         }       
+
         private void TestSave_Click(object sender, EventArgs e)
         {
-            SaveFileDialog sfd = new SaveFileDialog();
-            sfd.Filter = ".png | *.png";
-            if (sfd.ShowDialog() == DialogResult.OK)
-            {
+            index += 1;                                       
                 int width, height;
                 width = panel1.Width;
                 height = panel1.Height;
                 Bitmap bmp = new Bitmap(width, height);
-                panel1.DrawToBitmap(bmp, panel1.ClientRectangle);
-                bmp.Save(sfd.FileName);
-            }
+                panel1.DrawToBitmap(bmp, panel1.ClientRectangle);            
+                bmp.Save(path+"/"+index+".jpg", ImageFormat.Jpeg);
+            String fullAppName = Application.ExecutablePath;
+            String fullAppPath = Path.GetDirectoryName(fullAppName);
+            String fullFileName = Path.Combine(fullAppPath, "sound_046.wav");
+            SoundPlayer sp = new SoundPlayer(fullFileName);
+            sp.Play();
         }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.Description = "Выберите путь для сохранения файлов";
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Выбранный путь для автосохранения файлов: " + FBD.SelectedPath);
+            }
+            path = FBD.SelectedPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.Cancel)
+                return;           
+            string excelfile = ofd.FileName;
+            string fileText = File.ReadAllText(excelfile);
+            
+        }
+
+       
     }
 }
