327a8166b3270dc1221f87856df3a7e1561752d2
Danila200160 <Danila200160@yandex.ru>
2022-07-21 07:31:35 +0300

    Добавлена панель предпросмотра

--- WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
@@ -28,49 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Preview = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.Preview)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Preview
+            // pictureBox1
             // 
-            this.Preview.AllowUserToAddRows = false;
-            this.Preview.AllowUserToDeleteRows = false;
-            this.Preview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Preview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1});
-            this.Preview.Location = new System.Drawing.Point(48, 69);
-            this.Preview.Name = "Preview";
-            this.Preview.ReadOnly = true;
-            this.Preview.Size = new System.Drawing.Size(516, 313);
-            this.Preview.TabIndex = 0;
+            this.pictureBox1.Location = new System.Drawing.Point(172, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
-            // Column1
+            // button1
             // 
-            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.Column1.HeaderText = "Штрихкод";
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            this.Column1.Width = 62;
+            this.button1.Location = new System.Drawing.Point(172, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Предыдущая";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(442, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Следующая";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1253, 613);
-            this.Controls.Add(this.Preview);
+            this.ClientSize = new System.Drawing.Size(717, 411);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form2";
             this.Text = "Панель предпросмотра";
             this.Load += new System.EventHandler(this.Form2_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.Preview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView Preview;
-        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -16,10 +16,13 @@ namespace WindowsFormsApp1
         public Form2()
         {
             InitializeComponent();
-        }       
+        }
 
+        private int _currentImageId = 0;
+        private readonly List<FileInfo> _imagesList = new List<FileInfo>();
         private void Form2_Load(object sender, EventArgs e)
-        {
+        {           
+        _imagesList.AddRange(new DirectoryInfo(@"C:\Windows\Temp\Hidden").GetFiles("*.png", SearchOption.AllDirectories));
             //int Colvo;
             //Colvo = Directory.GetFiles(@"C:\Windows\Temp\Hidden").Length;           
             //DataGridViewImageColumn imageColumn = new DataGridViewImageColumn();
@@ -28,10 +31,22 @@ namespace WindowsFormsApp1
             //for (int i = 1; i < Colvo; i++)
             //{
             //    Preview.Rows[i].Cells[0].Value = new Bitmap(@"C:\Windows\Temp\Hidden\" + i + ".png");
-                //imageColumn.Image = Image.FromFile(@"C:\Windows\Temp\Hidden\" + i + ".png");
+            //imageColumn.Image = Image.FromFile(@"C:\Windows\Temp\Hidden\" + i + ".png");
             //}
-            
+            _currentImageId = 0;
+            pictureBox1.Load(_imagesList[_currentImageId].FullName);
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_currentImageId > 0)
+                pictureBox1.Load(_imagesList[--_currentImageId].FullName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_currentImageId < _imagesList.Count - 1)
+                pictureBox1.Load(_imagesList[++_currentImageId].FullName);
         }
     }
 }
